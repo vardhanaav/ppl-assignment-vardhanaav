3105fc8a48390d688f383645374af0ff42d11f5a
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2017-02-26 16:54:06 +0530

    Add files via upload

---
+++ Q2/Happyfy.java
@@ -0,0 +1,10 @@
+public class Happyfy {
+	String boyfriend, girlfriend;
+	double happiness;
+
+	Happyfy(String boyfriend, String girlfriend, double happiness) {
+		this.boyfriend = boyfriend;
+		this.girlfriend = girlfriend;
+		this.happiness = happiness;
+	}
+}
---
+++ Q2/q2.sh
@@ -0,0 +1,10 @@
+javac Boys.java
+javac EssentialGifts.java
+javac Girls.java
+javac Happyfy.java
+javac LuxuryGifts.java
+javac UtilityGifts.java
+javac q2.java
+java q2
+cat log.txt log1.txt log2.txt
+
